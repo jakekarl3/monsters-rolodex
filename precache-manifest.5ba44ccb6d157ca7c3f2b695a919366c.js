831cba39de304d74b8721ce98aa682ce49c35517
Jake Karl <31318360+jakekarl3@users.noreply.github.com>
2020-04-21 15:55:57 -0500

    Updates

---
+++ precache-manifest.5ba44ccb6d157ca7c3f2b695a919366c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f4ce40a5dfebe3fec9a76d410a95375",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "aa895c4fc6543f3154c0",
+    "url": "/monsters-rolodex/static/css/main.c55e92ea.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa895c4fc6543f3154c0",
+    "url": "/monsters-rolodex/static/js/main.a9fa4606.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
