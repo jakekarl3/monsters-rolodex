3bc536842713c6c2144171ad06119b121580e6f5
Jake Karl <31318360+jakekarl3@users.noreply.github.com>
2020-04-21 13:51:34 -0500

    Created CardList and Card components, added to App component, added search filter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+
+import { CardList } from './components/card-list/card-list.component';
+
 import './App.css';
 
 class App extends Component {
@@ -6,31 +9,32 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: 'Frankenstein',
-          id: 'asc1'
-        },
-        {
-          name: 'Dracula',
-          id: 'asr2'
-        },
-        {
-          name: 'Zombie',
-          id: 'asr3'
-        }
-      ]
+      monsters: [],
+      searchField: ''
     };
   }
 
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }))
+  }
+
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLocaleLowerCase())
+      );
+
     return (
       <div className="App">
-        {
-          this.state.monsters.map(monster => (
-            <h1 key={monster.id}>{monster.name}</h1>
-          ))
-        }
+        <input 
+          value={this.state.searchField}
+          type='search' 
+          placeholder="Search mosnters" 
+          onChange={e =>this.setState({ searchField: e.target.value })} />
+        {this.state.searchField}
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
